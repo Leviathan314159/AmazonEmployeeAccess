0264acad42175516720b15575a7f1dac3ee44f69
Levi Williams <85586951+Leviathan314159@users.noreply.github.com>
2023-10-16 13:51:08 -0600

    Random Forest Model

--- AmazonAnalysis.R
+++ AmazonAnalysis.R
@@ -7,14 +7,15 @@ library(vroom)
 # library(ggmosaic)
 library(tidyverse)
 library(embed)
+library(ranger)
 
 # Read in the data -----------------------------------
 base_folder <- "AmazonEmployeeAccess/"
 access_train <- vroom(paste0(base_folder, "train.csv"))
 access_test <- vroom(paste0(base_folder, "test.csv"))
 
-glimpse(access_train)
-glimpse(access_test)
+# glimpse(access_train)
+# glimpse(access_test)
 
 # Exploratory Data Analysis --------------------------
 # # Create dataset for exploration
@@ -82,9 +83,9 @@ access_recipe <- recipe(ACTION ~ ., data = access_train) |>
   step_other(all_nominal_predictors(), threshold = threshold_percent) |> # condenses categorical values that are less than 1% into an "other" category
   step_dummy(all_nominal_predictors()) # encode to dummy variables
 
-prepped_access_recipe <- prep(access_recipe)
-baked_access <- bake(prepped_access_recipe, new_data = access_train)
-glimpse(baked_access) # Check how many columns there are; should be 112
+# prepped_access_recipe <- prep(access_recipe)
+# baked_access <- bake(prepped_access_recipe, new_data = access_train)
+# glimpse(baked_access) # Check how many columns there are; should be 112
 
 
 # Recipe for penalized logsitic regression
@@ -93,62 +94,115 @@ penalized_logistic_recipe <- recipe(ACTION ~ ., data = access_train) |>
   step_other(all_nominal_predictors(), threshold = threshold_percent) |> 
   step_lencode_mixed(all_nominal_predictors(), outcome = vars(ACTION))
 
+# Recipe for random forest
+tree_recipe <- recipe(ACTION ~ ., data = access_train) |> 
+  step_mutate_at(all_numeric_predictors(), fn = factor) |> 
+  step_other(all_nominal_predictors(), threshold = threshold_percent) # |> 
+  # step_lencode_mixed(all_nominal_predictors(), outcome = vars(ACTION))
 
 # Logistic Regression Model -----------------------
-logistic_mod <- logistic_reg() |> set_engine("glm")
-
-logistic_amazon_wf <- workflow() |> 
-  add_recipe(access_recipe) |> 
-  add_model(logistic_mod) |> 
-  fit(data = access_train)
-
-logistic_amazon_pred <- predict(logistic_amazon_wf, 
-                                new_data = access_test,
-                                type = "prob")
-logistic_amazon_pred
-logistic_amazon_export <- data.frame("id" = 1:length(logistic_amazon_pred$.pred_1),
-                                     "Action" = logistic_amazon_pred$.pred_1)
-
+# logistic_mod <- logistic_reg() |> set_engine("glm")
+# 
+# logistic_amazon_wf <- workflow() |> 
+#   add_recipe(access_recipe) |> 
+#   add_model(logistic_mod) |> 
+#   fit(data = access_train)
+# 
+# logistic_amazon_pred <- predict(logistic_amazon_wf, 
+#                                 new_data = access_test,
+#                                 type = "prob")
+# logistic_amazon_pred
+# logistic_amazon_export <- data.frame("id" = 1:length(logistic_amazon_pred$.pred_1),
+#                                      "Action" = logistic_amazon_pred$.pred_1)
+# 
 # Penalized Logistic Regression Model -----------------------
-penalized_logistic_mod <- logistic_reg(mixture = tune(), penalty = tune()) |> 
-  set_engine("glmnet")
+# penalized_logistic_mod <- logistic_reg(mixture = tune(), penalty = tune()) |> 
+#   set_engine("glmnet")
+# 
+# penalized_amazon_wf <- workflow () |>
+#   add_recipe(penalized_logistic_recipe) |> 
+#   add_model(penalized_logistic_mod)
+# 
+# # Set the tuning grid
+# amazon_logistic_tuning_grid <- grid_regular(penalty(),
+#                                             mixture(),
+#                                             levels = 5)
+# 
+# # Set up the CV
+# penalized_amazon_folds <- vfold_cv(access_train, v = 10, repeats = 1)
+# 
+# # Run the CV
+# penalized_CV_results <- penalized_amazon_wf |> 
+#   tune_grid(resamples = penalized_amazon_folds,
+#             grid = amazon_logistic_tuning_grid,
+#             metrics = metric_set(roc_auc)) #, f_meas, sens, recall, spec,
+#                                  # precision, accuracy))
+# 
+# # Find out the best tuning parameters
+# best_tune <- penalized_CV_results |> select_best("roc_auc")
+# best_tune
+# 
+# # Use the best tuning parameters for the model
+# final_penalized_wf <- penalized_amazon_wf |> 
+#   finalize_workflow(best_tune) |> 
+#   fit(data = access_train)
+# 
+# # Predictions
+# penalized_logistic_preds <- final_penalized_wf |> 
+#   predict(new_data = access_test, type = "prob")
+# 
+# # Prepare export
+# penalized_export <- data.frame("id" = 1:length(penalized_logistic_preds$.pred_1),
+#                                "Action" = penalized_logistic_preds$.pred_1)
+# 
+# 
+# Random Forest (Classification) -----------------------------
+forest_amazon <- rand_forest(mtry = tune(),
+                             min_n = tune(),
+                             trees = 500) |> 
+  set_engine("ranger") |> 
+  set_mode("classification")
 
-penalized_amazon_wf <- workflow () |>
-  add_recipe(penalized_logistic_recipe) |> 
-  add_model(penalized_logistic_mod)
+# Create a workflow using the model and recipe
+forest_amazon_wf <- workflow() |> 
+  add_model(forest_amazon) |> 
+  add_recipe(tree_recipe)
 
-# Set the tuning grid
-amazon_logistic_tuning_grid <- grid_regular(penalty(),
-                                            mixture(),
-                                            levels = 5)
+# Set up the grid with the tuning values
+forest_amazon_grid <- grid_regular(mtry(range = c(1, (length(access_train)-1))), min_n())
 
-# Set up the CV
-penalized_amazon_folds <- vfold_cv(access_train, v = 10, repeats = 1)
+# Set up the K-fold CV
+forest_amazon_folds <- vfold_cv(data = access_train, v = 10, repeats = 1)
 
-# Run the CV
-penalized_CV_results <- penalized_amazon_wf |> 
-  tune_grid(resamples = penalized_amazon_folds,
-            grid = amazon_logistic_tuning_grid,
-            metrics = metric_set(roc_auc)) #, f_meas, sens, recall, spec,
-                                 # precision, accuracy))
+# Find best tuning parameters
+forest_cv_results <- forest_amazon_wf |> 
+  tune_grid(resamples = forest_amazon_folds,
+            grid = forest_amazon_grid,
+            metrics = metric_set(roc_auc))
+
+collect_metrics(forest_cv_results) |> 
+  filter(.metric == "rmse") %>% 
+  ggplot(data = ., aes(x = mtry, y = mean, color = factor(min_n))) + 
+  geom_point()
+
+# Finalize the workflow using the best tuning parameters and predict
+# The best parameters were mtry = 9 and min_n = 2
 
 # Find out the best tuning parameters
-best_tune <- penalized_CV_results |> select_best("roc_auc")
-best_tune
+best_forest_tune <- forest_cv_results |> select_best("roc_auc")
 
 # Use the best tuning parameters for the model
-final_penalized_wf <- penalized_amazon_wf |> 
-  finalize_workflow(best_tune) |> 
+forest_final_wf <- forest_amazon_wf |> 
+  finalize_workflow(best_forest_tune) |> 
   fit(data = access_train)
 
-# Predictions
-penalized_logistic_preds <- final_penalized_wf |> 
-  predict(new_data = access_test, type = "prob")
+forest_amazon_predictions <- predict(forest_final_wf, new_data = access_test)
+forest_amazon_predictions
 
-# Prepare export
-penalized_export <- data.frame("id" = 1:length(penalized_logistic_preds$.pred_1),
-                               "Action" = penalized_logistic_preds$.pred_1)
+forest_export <- data.frame("id" = 1:length(forest_amazon_predictions$.pred_1),
+                               "Action" = forest_amazon_predictions$.pred_1)
 
 # Write the data ---------------------------------
-vroom_write(logistic_amazon_export, paste0(base_folder, "logistic.csv"), delim = ",")
-vroom_write(penalized_export, paste0(base_folder, "penalized_logistic.csv"), delim = ",")
+# vroom_write(logistic_amazon_export, paste0(base_folder, "logistic.csv"), delim = ",")
+# vroom_write(penalized_export, paste0(base_folder, "penalized_logistic.csv"), delim = ",")
+vroom_write(forest_export, paste0(base_folder, "random_forest_classification.csv"), delim =",")
